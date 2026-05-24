c6586f990dcc7ee87c2c109e60d633ab28b33085
Giddy-Up224 <do-not-reply@no-kidding.com>
2026-05-23 20:17:12 -0400

    Fix: Fixed offset ccalculations

--- src/img_gen.py
+++ src/img_gen.py
@@ -33,12 +33,21 @@ import math
 
 class PositionAndSize:
     def __init__(self, canvas_size: list, arc_diameter: int, x_offset=0, y_offset=0):
+        self.canvas_size = canvas_size
+        self.arc_diameter = arc_diameter
         self.x_offset = x_offset
         self.y_offset = y_offset
-        self.left_margin = ((canvas_size[0] - arc_diameter) / 2) + self.x_offset
-        self.top_margin = ((canvas_size[1] - arc_diameter) / 2) + self.y_offset
-        self.right_margin = (canvas_size[0] - self.left_margin) + self.x_offset
-        self.bottom_margin = (canvas_size[1] - self.top_margin) + self.y_offset
+        self.update()
+        
+
+    def update(self):
+        width = (self.canvas_size[0] - self.arc_diameter) / 2
+        height = (self.canvas_size[1] - self.arc_diameter) / 2
+        self.left_margin = width + self.x_offset
+        self.top_margin = height + self.y_offset
+        self.right_margin = (self.canvas_size[0] - width) + self.x_offset
+        self.bottom_margin = (self.canvas_size[1] - height) + self.y_offset
+        
         self.coords = [
             self.left_margin,
             self.top_margin,
@@ -59,6 +68,8 @@ class ArcGenerator:
         self.start_angle     = 270
         self.end_angle       = 90
         self.arc_thickness   = 10
+        self.vert_offset     = 0
+        self.horiz_offset    = 0
 
     def set_arc_color(self, red, green, blue):
         self._arc_color = (red, green, blue)
@@ -72,6 +83,9 @@ class ArcGenerator:
         self.canvas     = Image.new('RGBA', self.canvas_size, self._cvs_color)
         self.draw       = ImageDraw.Draw(self.canvas)
         self.pos_n_size = PositionAndSize(self.canvas_size, self.arc_diameter)
+        self.pos_n_size.x_offset = self.horiz_offset
+        self.pos_n_size.y_offset = self.vert_offset
+        self.pos_n_size.update()
         self._start_angle = self.clock_to_pil_rotation(self.start_angle)
         print(f"Start angle (user): {self.start_angle}, PIL: {self._start_angle}")
         self._end_angle = self.clock_to_pil_rotation(self.end_angle)
@@ -85,8 +99,10 @@ def main():
     arc = ArcGenerator()
     arc.canvas_bg_color = (255, 255, 255)
     arc.opacity = 255
-    arc.start_angle = 235
-    arc.end_angle = 125
+    arc.start_angle = 0
+    arc.end_angle = 359
+    arc.vert_offset = 0
+    arc.horiz_offset = 0
     arc.create()
     arc.save('test/test.png')
 
