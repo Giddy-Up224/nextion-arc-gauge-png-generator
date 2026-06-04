18efa9b47834f9965fef717df26bbd56e3ee95e0
Giddy-Up224 <128929355+Giddy-Up224@users.noreply.github.com>
2026-06-04 07:18:32 -0400

    Fix pip upgrade command in virtual environment setup scripts

--- venv.ps1
+++ venv.ps1
@@ -2,5 +2,5 @@
 # This script creates a virtual environment and installs the required packages.
 python.exe -m venv venv
 venv/Scripts/Activate.ps1
-pip install --upgrade pip
+python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
--- venv.sh
+++ venv.sh
@@ -2,5 +2,5 @@
 # This script creates a virtual environment and installs the required packages.
 python3 -m venv venv
 source venv/bin/activate
-pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
